4060529334d5f588255a094a5d948e9aa0aac24b
DawidLycz <mamnie986@gmail.com>
2023-07-11 20:18:54 +0200

    restructurisation of reposytory

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='Hangman',
     version='0.1',
     packages=find_packages(),
-    scripts=['Hangman.py', 'Hangman_text_handler.py', "Hangman_numbers_loader.py"],
+    scripts=['hangman.py'],
     url='https://github.com/DawidLycz/Hungman',
     license='MIT',
     author='Dawid Lycz',
--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,4 +1,4 @@
-from hangman.hangman import check_mouse
+from hangman import check_mouse
 
 def test_check_mouse_if_in_button():
     position = (10,10)
@@ -12,18 +12,18 @@ def test_check_mouse_if_only_x_in_button():
     button_type = (100,100)
     mouse_pos = (21,300)
     got = check_mouse(position, button_type, mouse_pos)
-    assert got 
+    assert not got 
 
 def test_check_mouse_if_only_y_in_button():
     position = (10,10)
     button_type = (100,100)
     mouse_pos = (200,30)
     got = check_mouse(position, button_type, mouse_pos)
-    assert got 
+    assert not got 
 
 def test_check_mouse_if_out_of_button():
     position = (10,10)
     button_type = (100,100)
     mouse_pos = (200,300)    
     got = check_mouse(position, button_type, mouse_pos)
-    assert got 
+    assert not got 
